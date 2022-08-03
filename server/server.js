05ffb3feebf48307ebf22964f58cfad6f98ac00d
DukeHdez <briayan27@outlook.com>
2022-08-03 15:04:28 -0400

    add server handling file

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+const path = require('path');
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware
+});
+
+server.applyMiddleware({ app })
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+};
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API SERVER RUNNING ON PORT ${PORT}`);
+        console.log(`USE GRAPHQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+});
