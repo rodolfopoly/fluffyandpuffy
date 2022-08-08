4ee6910656c3d7cf7b530a4f2d89f35c00cc7d94
Megan Silva <megansilva@Megans-MacBook-Pro.local>
2022-08-08 19:18:19 -0400

    signup form

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -28,11 +28,44 @@ function Signup(props) {
       [name]: value,
     });
   };
-};
 
-  
+  return (
+    <div className="container my-1">
+      <Link to="/login">Login</Link>
 
+      <h2>Signup</h2>
+      <form onSumbit={handleFormSubmit}>
+      <div className="flex-row space-between my-2">
+          <label for="input-group-1" class="block mb-2 text-sm font-medium">Email</label>
+          <input
+            class="rounded-none rounded-r-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+            placeholder="Email">
+            onChange={handleChange}
+          </input> 
+        </div>
+        <div className="flex-row space-between my-2">
+        <label for="input-group-1" class="block mb-2 text-sm font-medium">Username</label>
+          <input
+            class="rounded-none rounded-r-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+            placeholder="Username">
+            onChange={handleChange}
+          </input>
+        </div>
+        <div className="flex-row space-between my-2">
+        <label for="input-group-1" class="block mb-2 text-sm font-medium">Password</label>
+          <input
+           class="rounded-none rounded-r-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+           placeholder="Password">
+            onChange={handleChange}
+          </input>
+        </div>
+        <div className="flex-row flex-end">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
 
-  
+}  
 
 export default Signup;
