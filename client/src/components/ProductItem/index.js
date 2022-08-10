028dc9efe05a9a9c9acc44125569018288024546
Rodofo Cabrera <rodolfopoly@Rodofos-Air.hitronhub.home>
2022-08-10 19:57:22 -0400

    add productItem component

---
+++ client/src/components/ProductItem/index.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { pluralize } from "../../utils/helpers"
+import { useStoreContext } from "../../utils/GlobalState";
+import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
+import { idbPromise } from "../../utils/helpers";
+
+function ProductItem(item) {
+  const [state, dispatch] = useStoreContext();
+
+  const {
+    image,
+    name,
+    _id,
+    price,
+  } = item;
+
+  const { cart } = state
+
+  const addToCart = () => {
+    const itemInCart = cart.find((cartItem) => cartItem._id === _id)
+    if (itemInCart) {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: _id,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
+      idbPromise('cart', 'put', {
+        ...itemInCart,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
+    } else {
+      dispatch({
+        type: ADD_TO_CART,
+        product: { ...item, purchaseQuantity: 1 }
+      });
+      idbPromise('cart', 'put', { ...item, purchaseQuantity: 1 });
+    }
+  }
+
+  return (
+    <div class="w-full max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <Link to={`/products/${_id}`}>
+        <img class="p-8 rounded-t-lg" src={`/images/${image}`} alt="product image"/>
+      </Link>
+      <div class="px-5 pb-5">
+        <a href="#">
+          <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{name}</h5>
+        </a>
+        <div class="flex justify-between items-center">
+          <span class="text-3xl font-bold text-gray-900 dark:text-white">${price}</span>
+          <button onClick={addToCart} class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProductItem;
