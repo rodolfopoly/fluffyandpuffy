796dcb2e1c5f41c93234d3721b3af954c1465fe5
ezravkatz <ezravkatz@gmail.com>
2022-08-12 18:41:16 -0400

    adding navbar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,1 +1,96 @@
-import React from 'react';
+import React from "react";
+import { Navbar } from "flowbite-react";
+import { Dropdown } from "flowbite-react";
+import { Avatar } from "flowbite-react";
+import { Button } from "flowbite-react";
+import CategoryMenu from "../CategoryMenu";
+function Nav() {
+    const hello = true;
+    if (hello) {
+      return (
+        <Navbar
+          fluid={true}
+          rounded={true}
+        >
+          <Navbar.Brand href="https://flowbite.com/">
+            <img
+              src="https://flowbite.com/docs/images/logo.svg"
+              className="mr-3 h-6 sm:h-9"
+              alt="Flowbite Logo"
+            />
+            <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+              Flowbite
+            </span>
+          </Navbar.Brand>
+          <div className="flex md:order-2">
+            <Dropdown
+              arrowIcon={false}
+              inline={true}
+              label={<Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded={true} />}
+            >
+              <Dropdown.Header>
+                <span className="block text-sm">
+                  Bonnie Green
+                </span>
+                <span className="block truncate text-sm font-medium">
+                  name@flowbite.com
+                </span>
+              </Dropdown.Header>
+              <Dropdown.Item>
+                Cart
+              </Dropdown.Item>
+              <Dropdown.Item>
+                Saved Items
+              </Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item>
+                Sign out
+              </Dropdown.Item>
+            </Dropdown>
+            <Navbar.Toggle />
+          </div>
+          <Navbar.Collapse>
+            <CategoryMenu/>
+          </Navbar.Collapse>
+        </Navbar>
+      )
+    } else {
+      return (
+        <Navbar
+          fluid={true}
+          rounded={true}
+        >
+          <Navbar.Brand href="https://flowbite.com/">
+            <img
+              src="https://flowbite.com/docs/images/logo.svg"
+              className="mr-3 h-6 sm:h-9"
+              alt="Flowbite Logo"
+            />
+            <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+              Flowbite
+            </span>
+          </Navbar.Brand>
+          <div className="flex md:order-2">
+            <Button color="purple">
+              Purple
+            </Button>
+          </div>
+          <Navbar.Collapse>
+            <Navbar.Link>
+              Cakes
+            </Navbar.Link>
+            <Navbar.Link href="/navbars">
+              Cookies
+            </Navbar.Link>
+            <Navbar.Link href="/navbars">
+              Cupcakes
+            </Navbar.Link>
+            <Navbar.Link href="/navbars">
+              Mini Desserts
+            </Navbar.Link>
+          </Navbar.Collapse>
+        </Navbar>
+      )
+    }
+  }
+export default Nav;
