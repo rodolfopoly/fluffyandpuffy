db19f878db3199ffaf0fee00d13623021b1ca418
Rodofo Cabrera <rodolfopoly@Rodofos-Air.hitronhub.home>
2022-08-15 19:07:41 -0400

    edit sing in

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
-//import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Button } from 'flowbite-react';
 import { Modal } from 'flowbite-react';
 import { Label } from 'flowbite-react';
 import { TextInput } from 'flowbite-react';
 import { LOGIN } from '../utils/mutations';
-import Signup from './Signup';
+//import Signup from './Signup';
 import Auth from '../utils/auth';
 
 function Login(props) {
@@ -43,65 +43,62 @@ function Login(props) {
 
   return (
     <React.Fragment>
-    <Button onClick={handleShow}>
-      Toggle modal
-    </Button>
-    <Modal
-      show={show}
-      size="md"
-      popup={true}
-      onClose={handleClose}
-    >
-      <Modal.Header />
-      <Modal.Body>
-        <div className="space-y-6 px-6 pb-4 sm:pb-6 lg:px-8 xl:pb-8">
-          <h3 className="text-xl font-medium text-gray-900 dark:text-white">
-            Sign in to our platform
-          </h3>
-          <div>
-            <div className="mb-2 block">
-              <Label
-                htmlFor="email"
-                value="Your email"
+      <Button onClick={handleShow}>
+        Login
+      </Button>
+      <Modal
+        show={show}
+        size="md"
+        popup={true}
+        onClose={handleClose}
+      >
+        <Modal.Header />
+        <Modal.Body>
+          <div className="space-y-6 px-6 pb-4 sm:pb-6 lg:px-8 xl:pb-8">
+            <h3 className="text-xl font-medium text-gray-900 dark:text-white">
+              Sign in to our platform
+            </h3>
+            <div>
+              <div className="mb-2 block">
+                <Label
+                  htmlFor="email"
+                  value="Your email"
+                />
+              </div>
+              <TextInput
+                id="email"
+                placeholder="name@company.com"
+                required={true}
               />
             </div>
-            <TextInput
-              id="email"
-              placeholder="name@company.com"
-              required={true}
-            />
-          </div>
-          <div>
-            <div className="mb-2 block">
-              <Label
-                htmlFor="password"
-                value="Your password"
+            <div>
+              <div className="mb-2 block">
+                <Label
+                  htmlFor="password"
+                  value="Your password"
+                />
+              </div>
+              <TextInput
+                id="password"
+                type="password"
+                required={true}
               />
             </div>
-            <TextInput
-              id="password"
-              type="password"
-              required={true}
-            />
+            <div className="w-full">
+              <Button>
+                Log in to your account
+              </Button>
+            </div>
+            <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
+              Not registered?{' '}
+              <Button>
+              <Link to="/Signup">Signup</Link>
+              </Button>              
+            </div>
           </div>
-          <div className="w-full">
-            <Button>
-              Log in to your account
-            </Button>
-          </div>
-          <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-            Not registered?{' '}
-            <a
-              href="/modal"
-              className="text-blue-700 hover:underline dark:text-blue-500"
-            >
-              <Signup/>
-            </a>
-          </div>
-        </div>
-      </Modal.Body>
-    </Modal>
-  </React.Fragment>
+        </Modal.Body>
+      </Modal>
+    </React.Fragment>
   );
 
 }
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,24 +1,27 @@
 import React, { useState } from 'react';
 //import { Link } from 'react-router-dom';
 import { Button } from 'flowbite-react';
-import { Modal } from 'flowbite-react';
+//import { Modal } from 'flowbite-react';
 import { Label } from 'flowbite-react';
 import { TextInput } from 'flowbite-react';
 import { useMutation } from '@apollo/client';
 import Auth from '../utils/auth';
 import { ADD_USER } from '../utils/mutations';
 
+import { Link } from 'react-router-dom';
+
 function Signup(props) {
-  const [formState, setFormState] = useState({ username: '', password: '' });
+  const [formState, setFormState] = useState({ email: '', password: '' });
   const [addUser] = useMutation(ADD_USER);
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
     const mutationResponse = await addUser({
       variables: {
-        username: formState.username,
         email: formState.email,
         password: formState.password,
+        firstName: formState.firstName,
+        lastName: formState.lastName,
       },
     });
     const token = mutationResponse.data.addUser.token;
@@ -33,75 +36,62 @@ function Signup(props) {
     });
   };
 
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
   return (
-    <React.Fragment>
-    <Button onClick={handleShow}>
-      Toggle modal
-    </Button>
-    <Modal
-      show={show}
-      size="md"
-      popup={true}
-      onClose={handleClose}
-    >
-      <Modal.Header />
-      <Modal.Body>
-        <div className="space-y-6 px-6 pb-4 sm:pb-6 lg:px-8 xl:pb-8">
-          <h3 className="text-xl font-medium text-gray-900 dark:text-white">
-            Sign in to our platform
-          </h3>
-          <div>
-            <div className="mb-2 block">
-              <Label
-                htmlFor="email"
-                value="Your email"
-              />
-            </div>
-            <TextInput
-              id="email"
-              placeholder="name@company.com"
-              required={true}
+    <div className='px-48 py-8'>
+      <form onSubmit={handleFormSubmit} className="flex flex-col gap-4">
+        <div>
+          <div className="mb-2 block">
+            <Label
+              htmlFor="username"
+              value="Your username"
             />
           </div>
-          <div>
-            <div className="mb-2 block">
-              <Label
-                htmlFor="password"
-                value="Your password"
-              />
-            </div>
-            <TextInput
-              id="password"
-              type="password"
-              required={true}
-            />
-          </div>
-          <div className="w-full">
-            <Button>
-              Log in to your account
-            </Button>
-          </div>
-          <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-            Not registered?{' '}
-            <a
-              href="/modal"
-              className="text-blue-700 hover:underline dark:text-blue-500"
-            >
-              <Signup/>
-            </a>
-          </div>
+          <TextInput
+            id="username"
+            type="username"
+            placeholder="fluffyandpuffy"
+            required={true}
+            shadow={true}
+            onChange={handleChange}
+          />
         </div>
-      </Modal.Body>
-    </Modal>
-  </React.Fragment>
+        <div>
+          <div className="mb-2 block">
+            <Label
+              htmlFor="email2"
+              value="Your email"
+            />
+          </div>
+          <TextInput
+            id="email2"
+            type="email"
+            placeholder="name@flowbite.com"
+            required={true}
+            shadow={true}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <div className="mb-2 block">
+            <Label
+              htmlFor="password2"
+              value="Your password"
+            />
+          </div>
+          <TextInput
+            id="password2"
+            type="password"
+            required={true}
+            shadow={true}
+            onChange={handleChange}
+          />
+        </div>
+        <Button type="submit">
+          Register new account
+        </Button>
+      </form>
+    </div>
   );
-
-
-}  
+}
 
 export default Signup;
