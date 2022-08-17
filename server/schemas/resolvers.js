fdcbb25ea9b94df96d6d0aa3994a38397018c88f
Rodofo Cabrera <rodolfopoly@Rodofos-Air.hitronhub.home>
2022-08-17 14:31:10 -0400

    stripe working

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -55,25 +55,31 @@ const resolvers = {
     },
     checkout: async (parent, args, context) => {
       const url = new URL(context.headers.referer).origin;
+      console.log(url);
       const order = new Order({ products: args.products });
-      const line_items = [];
-
       const { products } = await order.populate('products');
 
+      const line_items = [];
+
       for (let i = 0; i < products.length; i++) {
+        // generate product id
         const product = await stripe.products.create({
           name: products[i].name,
           images: [`${url}/images/${products[i].image}`]
         });
 
+        // generate price id using the product id
         const price = await stripe.prices.create({
           product: product.id,
           unit_amount: products[i].price * 100,
           currency: 'usd',
         });
 
+        // add price id to the line items array
         line_items.push({
-          price: price.id        });
+          price: price.id,
+          quantity: 1
+        });
       }
 
       const session = await stripe.checkout.sessions.create({
@@ -83,7 +89,7 @@ const resolvers = {
         success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${url}/`
       });
-
+      
       return { session: session.id };
     }
   },
@@ -115,7 +121,7 @@ const resolvers = {
     },
     updateProduct: async (parent, { _id, price }) => {
 
-      return await Product.findByIdAndUpdate(_id,  { price: price } , { new: true });
+      return await Product.findByIdAndUpdate(_id, { price: price }, { new: true });
     },
     login: async (parent, { username, password }) => {
       const user = await User.findOne({ username });
